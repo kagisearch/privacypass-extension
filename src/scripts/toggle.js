5f283d87b96aa90c579a48c1b51072aa67d18ed9
Fernando Virdia <fernando@kagi.com>
2025-03-18 00:33:53 +0000

    - attempt reloading double-spends in case they are one-off issues caused by updates or race conditions, instead of just stopping at an error screen - adjust token loading for /html/ pages

--- src/scripts/toggle.js
+++ src/scripts/toggle.js
@@ -12,7 +12,9 @@ import {
     unsetAntiFingerprintingRules,
     setLocaRedirectorHeader,
     unsetLocaRedirectorHeader,
-    selfRemovingUnsetRefererHeadersListener
+    selfRemovingUnsetRefererHeadersListener,
+    setHTMLIndexRedirector,
+    unsetHTMLIndexRedirector
 } from './headers.js'
 
 import {
@@ -53,7 +55,7 @@ async function checkingDoubleSpendListener(details) {
     const url = new URL(details.url);
     const scheme_domain_port = url.origin;
     const pathname = url.pathname; // comes with a leading /
-    const endpoint = (pathname == "/") ? `${scheme_domain_port}${pathname}|` : `${scheme_domain_port}${pathname}`;
+    const endpoint = (pathname == "/" || pathname.endsWith('/html')) ? `${scheme_domain_port}${pathname}|` : `${scheme_domain_port}${pathname}`;
     if (VERBOSE) {
         debug_log(`checkingDoubleSpendListener: ${details.statusCode} ${endpoint}`)
     }
@@ -62,7 +64,42 @@ async function checkingDoubleSpendListener(details) {
         if (VERBOSE) {
             debug_log(`> loading a new token for ${endpoint}`)
         }
+        // a token was double spent, load the next one
         await setPPHeaders(endpoint);
+        /*
+         * The status at this line is:
+         * - an error page is shown (or no results displayed in case of a /socket/ request failing)
+         * - a new token is loaded for the endpoint that last failed
+         * This may be a one-off error, eg due to an update in header rules, or a race condition.
+         * If that's the case, we would rather reload the page now that a new token was set.
+         * If the error has happened repeatedly though, this is not a good strategy,
+         * as it may single out that there is a failing user making repeated queries, and also
+         * loop infinitely.
+         * Our approach here is the following:
+         * - load the time of the last recorded double-spend
+         * - if very recent, then this is a repeated failure, don't reload;
+         *   just show the error telling the user to check the documentation and possibly report the failure
+         * - if the last error is not very recent, then automatically reload the page
+        */
+        const now = (new Date()).getTime(); // unix time in milliseconds
+        const { last_double_spend } = await browser.storage.local.get({ 'last_double_spend' : 0 });
+        const gap = now - last_double_spend;
+        await browser.storage.local.set({ 'last_double_spend' : now }); // update the last_double_spend information
+        if (gap > 60 * 1000) { // if the last seen doublespend was more than 1 minute ago
+            // likely one-off double-spend, reload the current page
+            if (VERBOSE) {
+                debug_log("checkingDoubleSpendListener: one-off double-spend, reloading");
+            }
+            const active_tabs_cur_window =  await chrome.tabs.query({active: true, currentWindow: true});
+            const cur_tab = active_tabs_cur_window[0];
+            if (cur_tab && cur_tab.id && cur_tab.id == details.tabId) {
+                await browser.tabs.reload(details.tabId, { bypassCache: true });
+            }
+        } else {
+            if (VERBOSE) {
+                debug_log("checkingDoubleSpendListener: repeated double-spend, not reloading");
+            }
+        }
     } else if (details.statusCode == 403) {
         // let the user know that their tokens are stale
         // realistically, this should only happen to devs debugging against staging
@@ -96,6 +133,7 @@ async function setEnabled() {
     await setRefererRules();
     await setAntiFingerprintingRules();
     await setLocaRedirectorHeader();
+    await setHTMLIndexRedirector();
     for (let i = 0; i < REDEMPTION_ENDPOINTS.length; i++) {
         let endpoint = REDEMPTION_ENDPOINTS[i];
         await setPPHeaders(endpoint);
@@ -123,6 +161,7 @@ async function setDisabled() {
     }
     await unsetAntiFingerprintingRules();
     await unsetLocaRedirectorHeader();
+    await unsetHTMLIndexRedirector();
     for (let i = 0; i < REDEMPTION_ENDPOINTS.length; i++) {
         let endpoint = REDEMPTION_ENDPOINTS[i];
         await unsetPPHeaders(endpoint);
